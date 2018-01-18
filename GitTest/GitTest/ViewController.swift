118d0ae9e73d23609590e26b92236335f446bf25
TomUser1 <TomUser1@gmailtest.com>
2018-01-18 14:39:13 +0800

    no message

--- GitTest/GitTest/ViewController.swift
+++ GitTest/GitTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //This is Tom add this line
     }
 
     override func didReceiveMemoryWarning() {
