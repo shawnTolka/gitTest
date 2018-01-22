0769d279dbdc734c07f8cece03466611482d87ce
TomUser1 <TomUser1@gmailtest.com>
2018-01-22 14:57:38 +0800

    修掉ＢＵＧ

--- GitTest/GitTest/ViewController2.swift
+++ GitTest/GitTest/ViewController2.swift
@@ -13,7 +13,7 @@ class ViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Tom")
-        failed
+        print("bug fixed")
         
         //mike
         // Do any additional setup after loading the view.
